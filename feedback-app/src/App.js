4a91c6dfc8727b1440ccf0797935c35f3c160c88
Adrian Ionut Patrascu <ap@greenbyte.dk>
2023-06-12 20:24:49 +0200

    app start
    initializing the project and creating the header component

---
+++ feedback-app/src/App.js
@@ -0,0 +1,17 @@
+import Header from "./components/Header"
+
+function App() {
+    return (
+      <>
+        <Header text={true} />
+        <div className="container">
+          <h1>Feedback App</h1>
+        </div>
+      </>
+    );
+}
+
+
+
+
+export default App
