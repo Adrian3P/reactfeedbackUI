724fe22ccb40a778c63105812fc3d59eb4cd2c7c
Adrian Ionut Patrascu <ap@greenbyte.dk>
2023-06-14 11:54:38 +0200

    components added/changed
    -added components fedbackitem, feedbcklist, feedbackstats, changed header, added card component, added feedbackdata file for the feedback cards

--- feedback-app/src/App.js
+++ feedback-app/src/App.js
@@ -1,17 +1,25 @@
-import Header from "./components/Header"
+import Header from "./components/Header";
+import FeedbackData from "./data/FeedbackData";
+import FeedbackList from "./components/FeedbackList";
+import FeedbackStats from "./components/FeedbackStats";
+import { useState } from "react";
 
 function App() {
-    return (
-      <>
-        <Header text={true} />
-        <div className="container">
-          <h1>Feedback App</h1>
-        </div>
-      </>
-    );
+  const [feedback, setFeedback] = useState(FeedbackData);
+  const deleteFeedback = (id) => {
+    if (window.confirm("Are you sure you want to delete?")) {
+      setFeedback(feedback.filter((item) => item.id !== id));
+    }
+  };
+  return (
+    <>
+      <Header />
+      <div className="container">
+        <FeedbackStats feedback={feedback} />
+        <FeedbackList feedback={feedback} handleDelete={deleteFeedback} />
+      </div>
+    </>
+  );
 }
 
-
-
-
-export default App
+export default App;
